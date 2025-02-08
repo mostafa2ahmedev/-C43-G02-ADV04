8c88ec9aa9c3ae995e2d44dd72450f7ada4cd285
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2025-02-08 22:50:21 +0200

    HashTable Operation

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,44 @@
-﻿namespace Demo
+﻿using System.Collections;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            #region HashTable Operation
+            //Hashtable ht = new Hashtable() {
+
+            //    //["Ahmed"]= 1,
+            //};
+            ////Add
+            //ht.Add("Mostafa", 1);
+            //ht.Add("Ahmed", 2);
+            //ht.Add("Ayman", 3);
+
+
+            ////Safe Add
+            //if (!ht.ContainsKey("Ahmed")) {
+            //    ht.Add("Ahmed", 2);
+            //}
+            //// Get
+            //foreach (DictionaryEntry item in ht)
+            //{
+            //    Console.WriteLine($"Key: {item.Key} ,value: {item.Value} ");
+            //}
+            //foreach (var key in ht.Keys)
+            //{
+            //    Console.WriteLine($"Key: {key} ");
+
+            //}
+            //foreach (var value in ht.Values)
+            //{
+            //    Console.WriteLine($"value: {value} ");
+            //} 
+            #endregion
+
         }
     }
 }
